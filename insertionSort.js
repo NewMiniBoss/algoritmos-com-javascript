e31117df9ab1686a3a83ef66bddbb8ba3fd4b44f
NewMiniBoss <gabriel.f.lazari@gmail.com>
2023-01-05 09:31:57 -0300

    refatorando código

--- insertionSort.js
+++ insertionSort.js
@@ -1,15 +1,10 @@
 const livros = require('./livros.json');
-
+const troca = require('./troca')
 function insertionSort(lista) {
     for (let i = 0; i < lista.length; i++) {
         let analise = i;
         while (analise > 0 && lista[analise].preco < lista[analise - 1].preco) {
-            let itemAnalise = lista[analise];
-            let itemAnterior = lista[analise - 1];
-
-            lista[analise] = itemAnterior;
-            lista[analise - 1] = itemAnalise;
-
+            troca(lista, analise)
             analise --;
         }
     }
